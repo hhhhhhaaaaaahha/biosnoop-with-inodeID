bd3b2934aaeb56fa32886d229386da691188a310
Ray <raytu.tpe@gmail.com>
2023-01-09 10:08:32 -0800

    update C code

--- biosnoop_C_Part.c
+++ biosnoop_C_Part.c
@@ -3,20 +3,28 @@
 #include <linux/fs.h>
 
 // bug fix
-struct start_req_t {
+struct start_req_t
+{
     u64 ts;
     u64 data_len;
 };
 // end of bug fix
 
-struct val_t {
+struct val_t
+{
     u64 ts;
     u32 pid;
-    u64 inode_id; // new line
     char name[TASK_COMM_LEN];
 };
 
-struct data_t {
+// added by Ray
+struct inode_t
+{
+    u64 inode_id[20];
+};
+
+struct data_t
+{
     u32 pid;
     u64 rwflag;
     u64 delta;
@@ -24,14 +32,36 @@ struct data_t {
     u64 sector;
     u64 len;
     u64 ts;
-    u64 inode_id; // new line
     char disk_name[DISK_NAME_LEN];
     char name[TASK_COMM_LEN];
+
+    // added by Ray
+    u64 inode_id_0;
+    u64 inode_id_1;
+    u64 inode_id_2;
+    u64 inode_id_3;
+    u64 inode_id_4;
+    u64 inode_id_5;
+    u64 inode_id_6;
+    u64 inode_id_7;
+    u64 inode_id_8;
+    u64 inode_id_9;
+    u64 inode_id_10;
+    u64 inode_id_11;
+    u64 inode_id_12;
+    u64 inode_id_13;
+    u64 inode_id_14;
+    u64 inode_id_15;
+    u64 inode_id_16;
+    u64 inode_id_17;
+    u64 inode_id_18;
+    u64 inode_id_19;
 };
 
 // BPF_HASH(start, struct request *); // bug fix
-BPF_HASH(start, struct request *, struct start_req_t); // new line
+BPF_HASH(start, struct request *, struct start_req_t); // bug fix
 BPF_HASH(infobyreq, struct request *, struct val_t);
+BPF_HASH(i_inobyreq, struct request *, struct inode_t); // added by Ray
 BPF_PERF_OUTPUT(events);
 
 // cache PID and comm by-req
@@ -40,10 +70,11 @@ int trace_pid_start(struct pt_regs *ctx, struct request *req)
     struct val_t val = {};
     u64 ts;
 
-    if (bpf_get_current_comm(&val.name, sizeof(val.name)) == 0) {
-        val.inode_id = req->bio->i_ino;
+    if (bpf_get_current_comm(&val.name, sizeof(val.name)) == 0)
+    {
         val.pid = bpf_get_current_pid_tgid() >> 32;
-        if (##QUEUE##) {
+        if (##QUEUE##)
+        {
             val.ts = bpf_ktime_get_ns();
         }
         infobyreq.update(&req, &val);
@@ -61,12 +92,32 @@ int trace_req_start(struct pt_regs *ctx, struct request *req)
     start.update(&req, &ts);
     */
 
+    // added by Ray
+    struct inode_t inode;
+    for (int i = 0; i < 20; i++)
+    {
+        inode.inode_id[i] = 999999999999999;
+    }
+    struct bio *tmp = req->bio;
+    for (int i = 0; i < 20; i++)
+    {
+        if (tmp != NULL)
+        {
+            inode.inode_id[i] = tmp->i_ino;
+            tmp = tmp->bi_next;
+        }
+        else
+        {
+            break;
+        }
+    }
+    i_inobyreq.update(&req, &inode);
+
+    // bug fix
     struct start_req_t start_req = {
         .ts = bpf_ktime_get_ns(),
-        .data_len = req->__data_len
-    };
+        .data_len = req->__data_len};
     start.update(&req, &start_req);
-    // end of bug fix
 
     return 0;
 }
@@ -77,6 +128,7 @@ int trace_req_completion(struct pt_regs *ctx, struct request *req)
     // u64 *tsp; // bug fix
     struct start_req_t *startp; // bug fix
     struct val_t *valp;
+    struct inode_t *inodep; // added by Ray
     struct data_t data = {};
     u64 ts;
 
@@ -84,7 +136,8 @@ int trace_req_completion(struct pt_regs *ctx, struct request *req)
     // tsp = start.lookup(&req);
     // if (tsp == 0) {
     startp = start.lookup(&req); // bug fix
-    if (startp == 0) {           // bug fix
+    if (startp == 0)
+    { // bug fix
         // missed tracing issue
         return 0;
     }
@@ -94,20 +147,51 @@ int trace_req_completion(struct pt_regs *ctx, struct request *req)
     data.ts = ts / 1000;
     data.qdelta = 0;
 
+    // added by Ray
+    inodep = i_inobyreq.lookup(&req);
+    if (inodep == 0)
+    {
+        // missed tracing issue
+        return 0;
+    }
+    data.inode_id_0 = inodep->inode_id[0];
+    data.inode_id_1 = inodep->inode_id[1];
+    data.inode_id_2 = inodep->inode_id[2];
+    data.inode_id_3 = inodep->inode_id[3];
+    data.inode_id_4 = inodep->inode_id[4];
+    data.inode_id_5 = inodep->inode_id[5];
+    data.inode_id_6 = inodep->inode_id[6];
+    data.inode_id_7 = inodep->inode_id[7];
+    data.inode_id_8 = inodep->inode_id[8];
+    data.inode_id_9 = inodep->inode_id[9];
+    data.inode_id_10 = inodep->inode_id[10];
+    data.inode_id_11 = inodep->inode_id[11];
+    data.inode_id_12 = inodep->inode_id[12];
+    data.inode_id_13 = inodep->inode_id[13];
+    data.inode_id_14 = inodep->inode_id[14];
+    data.inode_id_15 = inodep->inode_id[15];
+    data.inode_id_16 = inodep->inode_id[16];
+    data.inode_id_17 = inodep->inode_id[17];
+    data.inode_id_18 = inodep->inode_id[18];
+    data.inode_id_19 = inodep->inode_id[19];
+
     valp = infobyreq.lookup(&req);
     data.len = startp->data_len; // bug fix
-    if (valp == 0) {
+    if (valp == 0)
+    {
         // data.len = req->__data_len; // bug fix
         strcpy(data.name, "?");
-    } else {
-        if (##QUEUE##) {
+    }
+    else
+    {
+        if (##QUEUE##)
+        {
             // data.qdelta = *tsp - valp->ts; // bug fix
             data.qdelta = startp->ts - valp->ts; // bug fix
         }
         data.pid = valp->pid;
         // data.len = req->__data_len; // bug fix
         data.sector = req->__sector;
-        data.inode_id = valp->inode_id; // new line
         bpf_probe_read(&data.name, sizeof(data.name), valp->name);
         struct gendisk *rq_disk = req->rq_disk;
         bpf_probe_read(&data.disk_name, sizeof(data.disk_name),
